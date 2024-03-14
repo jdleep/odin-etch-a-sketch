b5b6fee5d4ffddd5afa4d879fcef9e5b5e5ce09d
Jeff Leep <3838327+jdleep@users.noreply.github.com>
2024-03-13 21:07:38 -0400

    Add mouseover event listener to turn squares black
    Fix inconsistencies in single quotes vs double quotes

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,12 +1,18 @@
 const container = document.querySelector('.squares-container');
 
-let squaresPerSide = 16;
+let squaresPerSide = 100;
 
 for (let i = 0; i < squaresPerSide ** 2; i++) {
     console.log(i);
     let squareDiv = document.createElement('div');
     squareDiv.classList.add('square');
-    squareDiv.style.width = 100 / squaresPerSide + "%";
-
+    squareDiv.style.width = 100 / squaresPerSide + '%';
     container.appendChild(squareDiv);
 } 
+
+
+container.addEventListener('mouseover', (e) => {
+    if (e.target.classList.contains('square')) {
+        e.target.classList.add('black');
+    }
+})
