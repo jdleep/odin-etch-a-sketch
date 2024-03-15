6f95512638444b75c3f3c383823e8c5ea45ffc71
Jeff Leep <3838327+jdleep@users.noreply.github.com>
2024-03-15 14:41:42 -0400

    Add "Create New Grid" button, hold down to draw
    Add Create New Grid button that allows you to
    reset grid with a custom number of squares
    per side.
    
    Add functionality to only draw while holding
    down mouse button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,18 +1,39 @@
 const container = document.querySelector('.squares-container');
 
-let squaresPerSide = 100;
+const INITIAL_SQUARES_PER_SIDE = 16;
 
-for (let i = 0; i < squaresPerSide ** 2; i++) {
-    console.log(i);
-    let squareDiv = document.createElement('div');
-    squareDiv.classList.add('square');
-    squareDiv.style.width = 100 / squaresPerSide + '%';
-    container.appendChild(squareDiv);
-} 
-
+function createGrid(container, squaresPerSide) {
+    for (let i = 0; i < squaresPerSide ** 2; i++) {
+        let squareDiv = document.createElement('div');
+        squareDiv.classList.add('square');
+        squareDiv.style.width = 100 / squaresPerSide + '%';
+        container.appendChild(squareDiv);
+    }    
+}
 
 container.addEventListener('mouseover', (e) => {
     if (e.target.classList.contains('square')) {
         e.target.classList.add('black');
     }
-})
+});
+
+
+const newGridButton = document.querySelector('.create-new-grid');
+newGridButton.addEventListener('click', (e) => {
+    let squaresPerSideStr = 
+        prompt('Choose number squares per side (must be an integer between 1 and 100)!', 
+        INITIAL_SQUARES_PER_SIDE);
+
+    if (squaresPerSideStr !== null) {
+        let squaresPerSide = +squaresPerSideStr;
+        if (Number.isInteger(squaresPerSide) && squaresPerSide <= 100 && squaresPerSide >= 1) {
+            container.replaceChildren();
+            createGrid(container, squaresPerSide);
+        } else {
+            alert('The value entered is not an integer between 1 and 100!');
+        }
+    }
+});
+
+
+createGrid(container, INITIAL_SQUARES_PER_SIDE);
