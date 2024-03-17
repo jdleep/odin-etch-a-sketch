ef5823c4d36daf87870889f4a4f708614e697aea
Jeff Leep <3838327+jdleep@users.noreply.github.com>
2024-03-17 17:39:19 -0400

    Add functionality to darken squares

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # odin-etch-a-sketch
 Odin Project Etch-a-Sketch assignment
+
+
+I used the following functions from stack overflow (modified to use arrays as 
+input):
+
+hsl2rgb
+https://stackoverflow.com/a/64090995/860099
+
+rgb2hsl
+https://stackoverflow.com/a/54071699/860099
--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.squares-container');
 
 const INITIAL_SQUARES_PER_SIDE = 16;
+const DARKEN_PERCENTAGE = 10;
 
 function createGrid(container, squaresPerSide) {
     for (let i = 0; i < squaresPerSide ** 2; i++) {
@@ -21,14 +22,53 @@ function createRandomRGB() {
     let g = getRandomInt(255);
     let b = getRandomInt(255);
 
-    return `rgb(${r},${g},${b})`;
+    return [r,g,b];
+}
+
+// in: r,g,b in [0,1], out: h in [0,360) and s,l in [0,1]
+function rgb2hsl(arrRGB) {
+    let r = arrRGB[0] /= 255;
+    let g = arrRGB[1] /= 255;
+    let b = arrRGB[2] /= 255;
+    let v=Math.max(r,g,b), c=v-Math.min(r,g,b), f=(1-Math.abs(v+v-c-1)); 
+    let h= c && ((v==r) ? (g-b)/c : ((v==g) ? 2+(b-r)/c : 4+(r-g)/c)); 
+    return [60*(h<0?h+6:h), f ? c/f : 0, (v+v-c)/2];
+  }
+
+// input: h as an angle in [0,360] and s,l in [0,1] - output: r,g,b in [0,1]
+function hsl2rgb(arrHSL) 
+{
+    let h = arrHSL[0];
+    let s = arrHSL[1];
+    let l = arrHSL[2];
+
+    let a=s*Math.min(l,1-l);
+    let f= (n,k=(n+h/30)%12) => l - a*Math.max(Math.min(k-3,9-k,1),-1);
+    return [f(0),f(8),f(4)];
+}   
+
+function darkenRGB(arrRGB, pctDarken) {
+    let arrHSL = rgb2hsl(arrRGB);
+    // darken by 10% (increase "l" value by 10)
+    arrHSL[2] = Math.max(0, arrHSL[2] -= pctDarken / 100);
+    return hsl2rgb(arrHSL).map((x) => x * 255);
 }
 
 container.addEventListener('mouseover', (e) => {
     if (e.target.classList.contains('square') && e.buttons === 1) {
+
+        // Default RGB color is white
+        let arrRGB = [255,255,255];
         // e.target.classList.add('black');
-        console.log(createRandomRGB());
-        e.target.style.backgroundColor = createRandomRGB();
+        // e.target.style.backgroundColor = 
+        // `rgb(${createRandomRGB().join(',')})`;
+        if (e.target.style.backgroundColor) {
+            console.log(e.target.style.backgroundColor.slice(4,-1));
+            arrRGB = e.target.style.backgroundColor.slice(4,-1).split(',');
+        }
+        arrRGB = darkenRGB(arrRGB, DARKEN_PERCENTAGE);
+        console.log(arrRGB.toString());
+        e.target.style.backgroundColor = `rgb(${arrRGB.join(',')})`;
     }
 });
 
